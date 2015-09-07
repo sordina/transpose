d61bb3c412c06bd1a123f4c0a197956b352c2e64
Lyndon Maydwell <lyndon@sordina.net>
2015-09-07 19:56:28 +1000

    Written

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # transpose
 
-A work in progress...
+Transpose rows and columns:
+
+    > transpose
+    a1,a2,a3
+    b1,b2,b3
+    c1,c2,c3
+    ^D
+
+    a1,b1,c1
+    a2,b2,c2
+    a3,b3,c3
+
+Usage:
+
+    > transpose -h
+
+    Usage: transpose [DELIM_IN] [DELIM_OUT] [-h|--help]
+
+
+You can do this with `hoe` if you're so inclined...
+
+    :'<,'>!hoe 'unlines . map unwords . transpose . map (splitOn ",") . lines' | tabulate ' '
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,24 @@
 module Main where
 
+import Data.List.Split
+import Data.List
+import System.IO
+import System.Exit
+import System.Environment
+
 main :: IO ()
-main = print "hello world"
+main = getArgs >>= go
+
+go ["-h"    ]  = help
+go ["--help"]  = help
+go ("--" :xs)  = go' xs
+go xs          = go' xs
+
+go' []     = interact (process "," ",")
+go' [i]    = interact (process i i)
+go' [i,o]  = interact (process i o)
+go' _      = help >> exitFailure
+
+process i o = unlines . map (intercalate o) . transpose . map (splitOn i) . lines
+
+help = putStrLn "Usage: transpose [DELIM_IN] [DELIM_OUT] [-h|--help]"
